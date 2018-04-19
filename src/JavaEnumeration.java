fa957b85260dd4e95166bcff2b1eca4c45793372
Souemn Mondal <soumen.mondal01@sap.com>
2018-04-19 16:51:08 +0530

    [Commit:2] Java enumeration example

---
+++ src/JavaEnumeration.java
@@ -0,0 +1,23 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+//Java enumeration used to use to retrived data from legacy java collection data structure i.e. vector and Hastable
+//This is only used in legacy java collection DS.
+//This can only be used to retrive data. this cannot delete data. This is like a forward iterating process
+public class JavaEnumeration {
+  public static void main(String[] args) {
+	  Vector<Integer> vector=new Vector();
+	  for(int i=0;i<10;i++) {
+		  vector.add(i);
+	  }
+	  
+	  //creating an enumeration of the vector object using ".elements()";
+	  Enumeration enumeration=vector.elements();
+	  
+	  //hasMoreElements() throws "no such Element" exception if there is no element present in the vector or in the collection 
+	  while(enumeration.hasMoreElements()) {
+		  System.out.println(enumeration.nextElement());
+	  }
+	  
+  }
+}
--- src/LegacyJavaCollection.java
+++ src/LegacyJavaCollection.java
@@ -12,12 +12,12 @@ import java.util.Vector;
 public class LegacyJavaCollection {
 	public static void main(String[] args) {
 		int[] array= {1,2,3,4,5,6};
-		Vector<Integer> vector=new Vector();
+		Vector vector=new Vector();
 		Hashtable<String,Integer> hashTable=new Hashtable<String,Integer>();
 		
-		//adding element to the vector
+		//adding element to the vector, you can add different data type data to vector
 		vector.add(1);
-		vector.add(2);
+		vector.add("2");
 		vector.add(3);
 		
 		//adding value to the Hashtable
@@ -29,7 +29,7 @@ public class LegacyJavaCollection {
 		System.out.println(array[0]);
 		
 		//retrive from vector
-		System.out.println(vector.get(0));
+		System.out.println(vector.get(1));
 		
 		//retrive from array
 		System.out.println(hashTable.get("one"));

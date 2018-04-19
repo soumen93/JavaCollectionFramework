603730d7d6287e566836062f884549944ad9e15d
Souemn Mondal <soumen.mondal01@sap.com>
2018-04-19 16:33:24 +0530

    [Commit-1]legacy Collection DS in java

---
+++ src/LegacyJavaCollection.java
@@ -0,0 +1,38 @@
+import java.util.Hashtable;
+import java.util.Vector;
+
+/**
+ * in java before collection framework introduced there were three collection DS available i.e.
+ * i)Array
+ * ii)Vector
+ * iii)Hashtable
+ * @author I332330
+ *
+ */
+public class LegacyJavaCollection {
+	public static void main(String[] args) {
+		int[] array= {1,2,3,4,5,6};
+		Vector<Integer> vector=new Vector();
+		Hashtable<String,Integer> hashTable=new Hashtable<String,Integer>();
+		
+		//adding element to the vector
+		vector.add(1);
+		vector.add(2);
+		vector.add(3);
+		
+		//adding value to the Hashtable
+		hashTable.put("one", 1);
+		hashTable.put("two", 2);
+		hashTable.put("three", 3);
+		
+		//retrive from array
+		System.out.println(array[0]);
+		
+		//retrive from vector
+		System.out.println(vector.get(0));
+		
+		//retrive from array
+		System.out.println(hashTable.get("one"));
+	}
+
+}
